172be47f51dcb607344434a8723421cfd7eb4a9f
aidancanavan <aidan.canavan3@gmail.com>
2023-11-25 00:30:24 -0500

    image updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -88,7 +88,7 @@ const Home = (props) => {
 
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row">
-                    <img src="https://s3.amazonaws.com/static.projectbly.com/uploads/photos/photo/filename/160/lookbook_photo.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                    <img src="https://storage.prompt-hunt.workers.dev/clg1va6fi000dmo089c1ytfbe_1" className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
                         <h1 className="text-5xl font-bold">Public Chat</h1>
                         <p className="py-6">No messages are never stored or cached. </p>
