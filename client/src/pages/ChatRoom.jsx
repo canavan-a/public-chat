eb66a36a3b15825fc2fc669b45b77a5daea437d5
canavan-a <aidan.canavan3@gmail.com>
2024-10-20 19:10:42 -0400

    added cicd for ec2

--- client/src/pages/ChatRoom.jsx
+++ client/src/pages/ChatRoom.jsx
@@ -1,6 +1,6 @@
-import { useState, useRef, useEffect } from 'react'
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ChatRoom = (props) => {
   const navigate = useNavigate();
@@ -10,31 +10,33 @@ const ChatRoom = (props) => {
   const scrollableContent = useRef(null);
 
   const queryParams = new URLSearchParams(location.search);
-  const room = queryParams.get("r")
+  const room = queryParams.get("r");
 
-  const username = queryParams.get("un")
+  const username = queryParams.get("un");
 
-  const [msg, setMsg] = useState("")
-  const [changeRoom, setChangeRoom] = useState(true)
+  const [msg, setMsg] = useState("");
+  const [changeRoom, setChangeRoom] = useState(true);
 
   const [chat, setChat] = useState([]);
 
   useEffect(() => {
-    console.log(username)
-    console.log(room)
-    console.log(window.location.hostname)
-    ws.current = new WebSocket(`wss://${window.location.hostname}/upgrade?username=${username}&room=${room}`);
+    console.log(username);
+    console.log(room);
+    console.log(window.location.hostname);
+    ws.current = new WebSocket(
+      `${wsApi}/upgrade?username=${username}&room=${room}`
+    );
     ws.current.onopen = () => {
-      console.log('WebSocket connection opened');
+      console.log("WebSocket connection opened");
     };
     ws.current.onmessage = (event) => {
-      console.log(event.data)
-      const newMsg = JSON.parse(event.data)
+      console.log(event.data);
+      const newMsg = JSON.parse(event.data);
 
-      setChat(prev => [...prev, newMsg]);
-    }
+      setChat((prev) => [...prev, newMsg]);
+    };
     ws.current.onclose = () => {
-      navigate("/")
+      navigate("/");
     };
     return () => {
       if (ws.current) {
@@ -43,11 +45,10 @@ const ChatRoom = (props) => {
     };
   }, [changeRoom, username, room]);
 
-
   const handleEnterKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       // If Enter key is pressed, trigger the button click
-      sendMessage()
+      sendMessage();
     }
   };
 
@@ -60,13 +61,14 @@ const ChatRoom = (props) => {
 
   const scrollToBottom = () => {
     if (scrollableContent.current) {
-      scrollableContent.current.scrollTop = scrollableContent.current.scrollHeight;
+      scrollableContent.current.scrollTop =
+        scrollableContent.current.scrollHeight;
     }
   };
 
   useEffect(() => {
-    scrollToBottom()
-  }, [chat])
+    scrollToBottom();
+  }, [chat]);
 
   return (
     <>
@@ -74,48 +76,80 @@ const ChatRoom = (props) => {
         <div className="hero-content flex-col lg:flex-col w-[100%] mx-auto ">
           <h1 className="text-5xl font-bold text-center">{room}</h1>
           <div className="card sm:w-[100%] lg:w-[50%] bg-base-100 shadow-xl">
-
             <div className="card-body">
               <div
                 ref={scrollableContent}
-                className="h-96 overflow-y-scroll overflow-x-hidden">
+                className="h-96 overflow-y-scroll overflow-x-hidden"
+              >
                 <div className="grid grid-cols-1">
                   {chat.map((value, index) => (
                     <div className="col">
                       {value.conclusion === "chat" ? (
-                        <div key={index} className={value.username === username ? "chat chat-end" : "chat chat-start flex flex-col"}>
-
-                          {value.username === username ? (<></>) : (<b className="badge badge-sm mb-1">{value.username}</b>)}
-                          <div className={`chat-bubble max-w-30 break-words ${value.username === username ? "chat-bubble-primary" : "chat-bubble-secondary"}`}>{value.msg}</div>
+                        <div
+                          key={index}
+                          className={
+                            value.username === username
+                              ? "chat chat-end"
+                              : "chat chat-start flex flex-col"
+                          }
+                        >
+                          {value.username === username ? (
+                            <></>
+                          ) : (
+                            <b className="badge badge-sm mb-1">
+                              {value.username}
+                            </b>
+                          )}
+                          <div
+                            className={`chat-bubble max-w-30 break-words ${
+                              value.username === username
+                                ? "chat-bubble-primary"
+                                : "chat-bubble-secondary"
+                            }`}
+                          >
+                            {value.msg}
+                          </div>
                         </div>
-                      ) : (<>
-                          {value.conclusion === "entering"?(<div className='text-center'><b>{value.username}</b> has joined</div>)
-                          :(<div className='text-center'><b>{value.username}</b> left</div>)}
-                          </>)}
+                      ) : (
+                        <>
+                          {value.conclusion === "entering" ? (
+                            <div className="text-center">
+                              <b>{value.username}</b> has joined
+                            </div>
+                          ) : (
+                            <div className="text-center">
+                              <b>{value.username}</b> left
+                            </div>
+                          )}
+                        </>
+                      )}
                     </div>
                   ))}
                 </div>
-
               </div>
               <div className="grid grid-cols-12 gap-x-2">
-                <input className="input input-bordered input-success input-md col-span-10"
+                <input
+                  className="input input-bordered input-success input-md col-span-10"
                   placeholder={"type something here"}
                   value={msg}
                   onKeyDown={handleEnterKeyPress}
-                  onChange={(e) => { setMsg(e.target.value) }}></input>
-                <button className='btn btn-primary col-span-2' onClick={sendMessage}>Send</button>
+                  onChange={(e) => {
+                    setMsg(e.target.value);
+                  }}
+                ></input>
+                <button
+                  className="btn btn-primary col-span-2"
+                  onClick={sendMessage}
+                >
+                  Send
+                </button>
               </div>
             </div>
-
-
           </div>
         </div>
       </div>
-
-
-
     </>
-  )
-}
+  );
+};
 
-export default ChatRoom 
+export default ChatRoom;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,140 +1,186 @@
-import { useState, useRef, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = (props) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const [wsApi, setWsApi] = useState(import.meta.env.VITE_WS_PATH);
+  const ws = useRef(null);
 
-    const [wsApi, setWsApi] = useState(import.meta.env.VITE_WS_PATH);
-    const ws = useRef(null);
+  const [changeRoom, setChangeRoom] = useState(true);
 
-    const [changeRoom, setChangeRoom] = useState(true);
+  const [rooms, setRooms] = useState([]);
 
-    const [rooms, setRooms] = useState([]);
+  const [connected, setConnected] = useState("pending");
 
-    const [connected, setConnected] = useState("pending");
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
 
-    const [username, setUsername] = useState("")
-    const [room, setRoom] = useState("")
+  const [usernameValid, setUsernameValid] = useState(true);
+  const [roomValid, setRoomValid] = useState(true);
+  const [firstPassUsername, setFirstPassUsername] = useState(true);
+  const [firstPassRoom, setFirstPassRoom] = useState(true);
 
-    const [usernameValid, setUsernameValid] = useState(true)
-    const [roomValid, setRoomValid] = useState(true)
-    const [firstPassUsername, setFirstPassUsername] = useState(true)
-    const [firstPassRoom, setFirstPassRoom] = useState(true)
+  const [toggle, setToggle] = useState(true);
 
-    const [toggle, setToggle] = useState(true);
-
-    useEffect(()=>{
-        if (!firstPassUsername){
-            if (username.length > 0){
-                setUsernameValid(true)
-            }
-            else{
-                setUsernameValid(false)
-            }
-        }
-        else{
-            setFirstPassUsername(false)
-        }
-    },[username, toggle])
-
-    useEffect(()=>{
-        if (!firstPassRoom){
-            if (room.length > 0){
-                setRoomValid(true)
-            }
-            else{
-                setRoomValid(false)
-            }
-        }
-        else{
-            setFirstPassRoom(false)
-        }
-    },[room])
-
-    useEffect(() => {
-        ws.current = new WebSocket(`wss://${window.location.hostname}/rooms`);
-        ws.current.onopen = () => {
-            setConnected(true);
-            console.log('WebSocket connection opened');
-        };
-        ws.current.onmessage = (event) => {
-            console.log("message received");
-            console.log(JSON.parse(event.data))
-            setRooms(JSON.parse(event.data));
-        }
-        ws.current.onclose = () => {
-            setConnected(false);
-            console.log("connection closed");
-        };
-        return () => {
-            if (ws.current) {
-                ws.current.close();
-            }
-        };
-    }, [changeRoom]);
-
-    const enterRoom = () =>{
-        if(username.length > 0 && room.length > 0){
-            navigate(`/chat?r=${room}&un=${username}`)
-        }
-        else{
-            
-        }
-        
+  useEffect(() => {
+    if (!firstPassUsername) {
+      if (username.length > 0) {
+        setUsernameValid(true);
+      } else {
+        setUsernameValid(false);
+      }
+    } else {
+      setFirstPassUsername(false);
     }
+  }, [username, toggle]);
 
-    return (
-        <>
+  useEffect(() => {
+    if (!firstPassRoom) {
+      if (room.length > 0) {
+        setRoomValid(true);
+      } else {
+        setRoomValid(false);
+      }
+    } else {
+      setFirstPassRoom(false);
+    }
+  }, [room]);
 
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row">
-                    <img src="https://storage.prompt-hunt.workers.dev/clg1va6fi000dmo089c1ytfbe_1" className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 className="text-5xl font-bold">Public Chat</h1>
-                        <p className="py-6">Messages never stored or cached. </p>
-                        <div className="grid grid-cols-4 max-w-md gap-4">
-                            <div className="grid grid-cols-6 gap-4 col-span-2">
-                                <input className={`input input-bordered input-sm col-span-6 ${usernameValid?'':'input-error'}`} value={username} onChange={(e)=>{setUsername(e.target.value)}} placeholder='username'></input>
-                                <input className={`input input-bordered input-sm col-span-6 ${roomValid?'':'input-error'}`} value={room} onChange={(e)=>{setRoom(e.target.value)}} placeholder='room'></input>
-                                {/* <div className="col-span-2"></div> */}
-                                {connected==="pending"?(<button className="btn btn-glass btn-sm col-span-3">loading</button>):(
-                                <>{connected ? (
-                                    <button className="btn btn-primary btn-sm col-span-3" onClick={enterRoom}>Go</button>
-                                ) : (
-                                    <button className="btn btn-warning btn-sm col-span-3" onClick={() => { setChangeRoom(!changeRoom) }}>reconnect</button>
-                                )}
-                                </>)}
-                            </div>
-                            {rooms.length !== 0?(
-                            <div className="simplebar grid grid-cols-6 gap-1 col-span-2 overflow-y-scroll overflow-x-hidden max-h-40 ">
-                            
-                                    {rooms.map((value,index)=>(
-                                        <RoomInstance key={value} setFirstPassUsername={setFirstPassUsername} name={value} room={room} setRoom={setRoom} toggle={toggle} setToggle={setToggle} />
-                                    ))}
+  useEffect(() => {
+    ws.current = new WebSocket(`${wsApi}/rooms`);
+    ws.current.onopen = () => {
+      setConnected(true);
+      console.log("WebSocket connection opened");
+    };
+    ws.current.onmessage = (event) => {
+      console.log("message received");
+      console.log(JSON.parse(event.data));
+      setRooms(JSON.parse(event.data));
+    };
+    ws.current.onclose = () => {
+      setConnected(false);
+      console.log("connection closed");
+    };
+    return () => {
+      if (ws.current) {
+        ws.current.close();
+      }
+    };
+  }, [changeRoom]);
 
-                            </div>
-                            ):(<></>)}
+  const enterRoom = () => {
+    if (username.length > 0 && room.length > 0) {
+      navigate(`/chat?r=${room}&un=${username}`);
+    } else {
+    }
+  };
 
-                        </div>
-
-
-                    </div>
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src="https://storage.prompt-hunt.workers.dev/clg1va6fi000dmo089c1ytfbe_1"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Public Chat</h1>
+            <p className="py-6">Messages never stored or cached. </p>
+            <div className="grid grid-cols-4 max-w-md gap-4">
+              <div className="grid grid-cols-6 gap-4 col-span-2">
+                <input
+                  className={`input input-bordered input-sm col-span-6 ${
+                    usernameValid ? "" : "input-error"
+                  }`}
+                  value={username}
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                  placeholder="username"
+                ></input>
+                <input
+                  className={`input input-bordered input-sm col-span-6 ${
+                    roomValid ? "" : "input-error"
+                  }`}
+                  value={room}
+                  onChange={(e) => {
+                    setRoom(e.target.value);
+                  }}
+                  placeholder="room"
+                ></input>
+                {/* <div className="col-span-2"></div> */}
+                {connected === "pending" ? (
+                  <button className="btn btn-glass btn-sm col-span-3">
+                    loading
+                  </button>
+                ) : (
+                  <>
+                    {connected ? (
+                      <button
+                        className="btn btn-primary btn-sm col-span-3"
+                        onClick={enterRoom}
+                      >
+                        Go
+                      </button>
+                    ) : (
+                      <button
+                        className="btn btn-warning btn-sm col-span-3"
+                        onClick={() => {
+                          setChangeRoom(!changeRoom);
+                        }}
+                      >
+                        reconnect
+                      </button>
+                    )}
+                  </>
+                )}
+              </div>
+              {rooms.length !== 0 ? (
+                <div className="simplebar grid grid-cols-6 gap-1 col-span-2 overflow-y-scroll overflow-x-hidden max-h-40 ">
+                  {rooms.map((value, index) => (
+                    <RoomInstance
+                      key={value}
+                      setFirstPassUsername={setFirstPassUsername}
+                      name={value}
+                      room={room}
+                      setRoom={setRoom}
+                      toggle={toggle}
+                      setToggle={setToggle}
+                    />
+                  ))}
                 </div>
+              ) : (
+                <></>
+              )}
             </div>
-        </>)
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
+const RoomInstance = (props) => {
+  const { name } = props;
+  const { room, setRoom } = props;
+  const { toggle, setToggle } = props;
+  const { setFirstPassUsername } = props;
 
-const RoomInstance = (props) =>{
-    const {name} = props;
-    const {room, setRoom} = props;
-    const {toggle, setToggle} = props;
-    const {setFirstPassUsername} = props
-
-    return(
-        <button onClick={()=>{setRoom(name); setFirstPassUsername(false); setToggle(!toggle);}} className={`btn btn-outline btn-sm mr-4 col-span-6 ${ room === name ?"btn-primary":""}`}>{name}</button>
-    )
-}
+  return (
+    <button
+      onClick={() => {
+        setRoom(name);
+        setFirstPassUsername(false);
+        setToggle(!toggle);
+      }}
+      className={`btn btn-outline btn-sm mr-4 col-span-6 ${
+        room === name ? "btn-primary" : ""
+      }`}
+    >
+      {name}
+    </button>
+  );
+};
 
 export default Home;
